e6ffe40068e9c354357aaee9f6b6c13e58928e3f
Owen O'Donnell <oodonne@tcd.ie>
2022-05-23 00:06:30 +0100

    updating login and signup components

--- src/pages/components/LoginForm.svelte
+++ src/pages/components/LoginForm.svelte
@@ -1,24 +1,40 @@
 <script>
   import {push} from "svelte-spa-router";
+  import {getContext} from "svelte";
 
   let email = ""
   let password = "";
+  let errorMessage = "";
+
+  const placemarkService = getContext("PlacemarkService");
 
   async function login() {
-    push("/dashboard");
+    let success = await placemarkService.login(email, password)
+    if (success) {
+      push("/dashboard");
+    } else {
+      email = "";
+      password = "";
+      errorMessage = "Invalid Credentials";
+    }
   }
 </script>
 
 <form on:submit|preventDefault={login}>
   <div class="field">
     <label class="label" for="email">Email</label>
-    <input bind:value={email} class="input" id="email" type="text" placeholder="Enter email" name="email">
+    <input bind:value={email} class="input" id="email" name="email"placeholder="Enter email" type="text">
   </div>
   <div class="field">
     <label class="label" for="password">Password</label>
-    <input bind:value={password} class="input" id="password" type="password" placeholder="Enter Password" name="password">
+    <input bind:value={password} class="input" id="password" name="password" placeholder="Enter Password" type="password">
   </div>
   <div class="field is-grouped">
-    <button class="button is-link">Log in</button>
+    <button class="button is-link">Log In</button>
   </div>
 </form>
+{#if errorMessage}
+  <div class="section">
+    {errorMessage}
+  </div>
+{/if}
--- src/pages/components/SignupForm.svelte
+++ src/pages/components/SignupForm.svelte
@@ -1,38 +1,52 @@
-<script lang="ts">
+<script>
   import { push } from "svelte-spa-router";
+  import { getContext } from "svelte";
 
   let firstName = "";
   let lastName = "";
   let email = ""
   let password = "";
+  let errorMessage = "";
+
+  const placemarkService = getContext("PlacemarkService");
 
   async function signup() {
-    push("/");
+    let success = await placemarkService.signup(firstName, lastName, email, password)
+    if (success) {
+      push("/");
+    } else {
+      errorMessage = "Error Trying to sign up";
+    }
   }
 </script>
 
 <form on:submit|preventDefault={signup}>
-  <div class="field is-horizontal">
-    <div class="field-body">
-      <div class="field">
-        <label for="firstname" class="label">First Name</label>
-        <input bind:value={firstName} class="input" id="firstname" type="text" placeholder="Enter first name" name="firstName">
-      </div>
-      <div class="field">
-        <label for="lastname" class="label">Last Name </label>
-        <input bind:value={lastName} class="input" id="lastname" type="text" placeholder="Enter last name" name="lastName">
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label for="firstname" class="label">First Name</label>
+          <input bind:value={firstName} id="firstname" class="input" type="text" placeholder="Enter first name" name="firstName">
+        </div>
+        <div class="field">
+          <label for="lastname" class="label">Last Name </label>
+          <input bind:value={lastName}  id="lastname" class="input" type="text" placeholder="Enter last name" name="lastName">
+        </div>
       </div>
     </div>
-  </div>
-  <div class="field">
-    <label for="email" class="label">Email</label>
-    <input bind:value={email} class="input" id="email" type="text" placeholder="Enter email" name="email">
-  </div>
-  <div class="field">
-    <label for="password" class="label">Password</label>
-    <input bind:value={password} class="input" id="password"type="password" placeholder="Enter Password" name="password">
-  </div>
-  <div class="field is-grouped">
-    <button class="button is-link">Sign Up</button>
-  </div>
+    <div class="field">
+      <label for="email" class="label">Email</label>
+      <input bind:value={email} id="email" class="input" type="text" placeholder="Enter email" name="email">
+    </div>
+    <div class="field">
+      <label for="password" class="label">Password</label>
+      <input bind:value={password} id="password" class="input" type="password" placeholder="Enter Password" name="password">
+    </div>
+    <div class="field is-grouped">
+      <button class="button is-link">Sign Up</button>
+    </div>
 </form>
+{#if errorMessage}
+  <div class="section">
+    {errorMessage}
+  </div>
+{/if}
