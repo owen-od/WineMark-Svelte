7fc79b92f65ee629e7540980a6fab46681185d9b
Owen O'Donnell <oodonne@tcd.ie>
2022-05-23 01:40:00 +0100

    add user store and local storage of user details

--- src/pages/components/TitleBar.svelte
+++ src/pages/components/TitleBar.svelte
@@ -1,4 +1,5 @@
 <script>
+  import {user} from "../../stores.js"
   export let title = "";
   export let subTitle = "";
 </script>
@@ -12,7 +13,11 @@
     <div class="subtitle is-5"> {subTitle} </div>
   </div>
   <div class="column">
-    <i class="fas fa-donate fa-3x" style="color:rgb(95, 96, 173)"></i>
-    <div class="is-size-7">WineMark-Svelte 0.1</div>
-  </div>
+    <i class="fas fa-donate fa-3x" style="color:rgb(95, 96, 173)" title="Source repo" pos="bottom" uk-tooltip></i>
+    {#if $user.email}
+      <div class="is-size-7">{$user.email} </div>
+    {:else}
+      <div class="is-size-7">Winemark-Svelte 0.2</div>
+    {/if}
+</div>
 </div>
--- src/services/PlacemarkService.js
+++ src/services/PlacemarkService.js
@@ -1,17 +1,34 @@
 import axios from "axios";
+import {user} from "../stores.js"
 
 export class PlacemarkService {
   baseUrl = "";
 
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
+    const winemarkCredentials = localStorage.winemark
+    if (winemarkCredentials) {
+      const savedUser = JSON.parse(winemarkCredentials)
+      user.set({
+        email: savedUser.email,
+        token: savedUser.token,
+        _id: savedUser._id,
+      });
+      axios.defaults.headers.common["Authorization"] = "Bearer " + savedUser.token;
+    }
   }
-
+  
   async login(email, password) {
     try {
       const response = await axios.post(`${this.baseUrl}/api/users/authenticate`, {email, password});
       axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.token;
       if (response.data.success) {
+        user.set({
+          email: email,
+          token: response.data.token,
+          _id: response.data._id,
+        });
+        localStorage.winemark = JSON.stringify({email:email, token:response.data.token, _id: response.data._id});
         return true;
       }
       return false;
@@ -21,7 +38,13 @@ export class PlacemarkService {
   }
 
   async logout() {
+    user.set({
+      email: "",
+      token: "",
+      _id: "",
+    });
     axios.defaults.headers.common["Authorization"] = "";
+    localStorage.removeItem("donation");
   }
 
   async signup(firstName, lastName, email, password) {
